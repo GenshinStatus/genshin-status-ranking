9856009953edde9039d6153ef2d8b7bffbbc60b0
chan-mai <yashinomi0804@gmail.com>
2024-05-15 23:41:49 +0900

    fix: うごくように

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,10 @@ const schema = z.object({
 	element_hurt_percent: z.number(),
 })
 
-
+/*
+	ランキングの生成 POST /api/gen_ranking
+	@param uid: number, hp: number, attack: number, defense: number, element_mastery: number, critical_percent: number, critical_hurt_percent: number, element_charge_efficiency_percent: number, element_hurt_percent: number
+*/
 app.post("/api/gen_ranking", zValidator('json', schema), async (c) => {
 	const body = await c.req.json()
 	const { uid, hp, attack, defense, element_mastery, critical_percent, critical_hurt_percent, element_charge_efficiency_percent, element_hurt_percent } = body
@@ -61,7 +64,7 @@ app.post("/api/gen_ranking", zValidator('json', schema), async (c) => {
 
 		if (now - update_at > 3600000) {
 			// 1時間以上経過しているデータは更新
-			const stmt = await c.env.DB.prepare("UPDATE userdata SET hp = ?, attack = ?, defense = ?, element_mastery = ?, critical_percent = ?, critical_hurt_percent = ?, element_charge_efficiency_percent = ?, element_hurt_percent = ? WHERE uid = ?");
+			const stmt = await c.env.DB.prepare("UPDATE userdata SET hp = ?, attack = ?, defense = ?, element_mastery = ?, critical_percent = ?, critical_hurt_percent = ?, element_charge_efficiency_percent = ?, element_hurt_percent = ?, updated_at = DATETIME('now', 'localtime') WHERE uid = ?");
 			await stmt.bind(hp, attack, defense, element_mastery, critical_percent, critical_hurt_percent, element_charge_efficiency_percent, element_hurt_percent, uid).run();
 		}
 	}
@@ -69,15 +72,24 @@ app.post("/api/gen_ranking", zValidator('json', schema), async (c) => {
 	// ランキングの生成
 	try {
 		// 当該uidの相対ランキングを取得
-		const stmt = c.env.DB.prepare("SELECT *, DENSE_RANK() over(order by hp, attack, defense, element_mastery, critical_percent, critical_hurt_percent, element_charge_efficiency_percent, element_hurt_percent) AS `rank`, COUNT( * ) AS `all_user_count` FROM userdata WHERE uid = ?");
-		const allResults = await stmt.bind(uid).all();
+		const stmt = c.env.DB.prepare("SELECT uid, hp, attack, defense, element_mastery, critical_percent, critical_hurt_percent, element_charge_efficiency_percent, element_hurt_percent, DENSE_RANK() over(ORDER BY hp DESC, attack DESC, defense DESC, element_mastery DESC, critical_percent DESC, critical_hurt_percent DESC, element_charge_efficiency_percent DESC, element_hurt_percent DESC) AS `rank`, updated_at, created_at FROM userdata");
+		const allResults = await stmt.all();
 
-		return c.json({ status: "success", message: "Ranking Generated", data: allResults.results[0] });
+		const all_user_count = allResults.length;
+
+		// 取得結果から当該uidを探索
+		for (let i = 0; i < allResults.results.length; i++) {
+			if (allResults.results[i].uid === uid) {
+				// all_user_countの追加
+				let data = allResults.results[i];
+				data["all_user_count"] = all_user_count;
+				return c.json({ status: "success", message: "Ranking Generated", data: data });
+			}
+		}
 	} catch (e: unknown) {
 		console.log(e);
 		return c.json({ status: "error", message: "Internal Server Error" });
 	}
-
 });
 
 
