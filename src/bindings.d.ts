9b5de5878fe37a48025b9000be9b98277ca7e6d9
chan-mai <yashinomi0804@gmail.com>
2024-05-15 14:12:12 +0900

    add: cfKV

---
+++ src/bindings.d.ts
@@ -0,0 +1,3 @@
+export interface Bindings {
+	userdata: KVNamespace;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,8 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Bindings } from "./bindings";
+import { Hono } from "hono";
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+const app = new Hono<Bindings>();
+
+app.get('/', (c) => c.text('Hello! it\'s Works!'));
+
+app.fire();
