1b7266157296fa614cf16d46e42bce7304cb9d18
chan-mai <yashinomi0804@gmail.com>
2024-05-16 18:31:27 +0900

    update: 平均値でランキングスコアを算出するように

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import { Hono } from "hono";
-import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
 import { Env as BaseEnv } from "hono/dist/types/types";
 
 type Env = BaseEnv & {
@@ -9,35 +7,33 @@ type Env = BaseEnv & {
 
 const app = new Hono<Env>();
 
-/*
-HP : hp
-攻撃力 : attack
-防御力 : defense
-元素熟知 : element_mastery
-会心率 : critical_percent
-会心ダメージ : critical_hurt_percent
-元素チャージ効率 : element_charge_efficiency_percent
-任意元素ダメージ : element_hurt_percent
-*/
-const schema = z.object({
-	uid: z.number(),
-	hp: z.number(),
-	attack: z.number(),
-	defense: z.number(),
-	element_mastery: z.number(),
-	critical_percent: z.number(),
-	critical_hurt_percent: z.number(),
-	element_charge_efficiency_percent: z.number(),
-	element_hurt_percent: z.number(),
-})
 
 /*
 	ランキングの生成 POST /api/gen_ranking
 	@param uid: number, hp: number, attack: number, defense: number, element_mastery: number, critical_percent: number, critical_hurt_percent: number, element_charge_efficiency_percent: number, element_hurt_percent: number
 */
-app.post("/api/gen_ranking", zValidator('json', schema), async (c) => {
-	const body = await c.req.json()
-	const { uid, hp, attack, defense, element_mastery, critical_percent, critical_hurt_percent, element_charge_efficiency_percent, element_hurt_percent } = body
+app.get("/api/gen_ranking/:uid",  async (c) => {
+	const uid:number = Number(c.req.param("uid"));
+	let hp: number = 0;
+	let attack: number = 0;
+	let defense: number = 0;
+	let element_mastery: number = 0;
+	let critical_percent: number = 0;
+	let critical_hurt_percent: number = 0;
+	let element_charge_efficiency_percent: number = 0;
+	let element_hurt_percent: number = 0;
+
+	// ユーザーのデータを取得
+	const averageData = await getAverageStatus(uid);
+	console.log(averageData);
+	hp = averageData.hp;
+	attack = averageData.attack;
+	defense = averageData.defense;
+	element_mastery = averageData.element_mastery;
+	critical_percent = averageData.critical_percent;
+	critical_hurt_percent = averageData.critical_hurt_percent;
+	element_charge_efficiency_percent = averageData.element_charge_efficiency_percent;
+	element_hurt_percent = averageData.element_hurt_percent;
 
 	console.log(uid, hp, attack, defense, element_mastery, critical_percent, critical_hurt_percent, element_charge_efficiency_percent, element_hurt_percent);
 
@@ -134,5 +130,114 @@ app.get("/api/get_report", async (c) => {
 		return c.json({ status: "error", message: "Internal Server Error" });
 	}
 });
+
+
+
+async function getAverageStatus(uid: number) {
+	// 当該uidの詳細取得
+	const response = await fetch(`https://enka.network/api/uid/${uid}`, {
+		method: 'GET',
+		headers: {
+			"User-Agent": "Genshin-Status-Ranking/1.6.7 (Linux;Ubuntu22.04; discord:@mai_llj)"
+		}
+	}
+	);
+	const data: any = await response.json();
+	// 各値の処理
+	const nickname = data["playerInfo"]["nickname"];
+	let avaterHpList: any = {};
+	let avaterAttackPowerList: any = {};
+	let avaterDefensePowerList: any = {};
+	let avaterElementMasteryList: any = {};
+	let avaterCriticalPercentList: any = {};
+	let avaterCriticalHurtPercentList: any = {};
+	let avaterElementChargeEfficiencyPercentList: any = {};
+	let avaterElementHurtPercentList: any = {};
+
+	// data["avatarInfoList"]の中身を順次処理
+	data["avatarInfoList"].forEach((element: any) => {
+		const avaterId = element["avatarId"];
+		// HP
+		avaterHpList[avaterId] = element["fightPropMap"][2000];
+		// 攻撃力
+		avaterAttackPowerList[avaterId] = element["fightPropMap"][2001];
+		// 防御力
+		avaterDefensePowerList[avaterId] = element["fightPropMap"][2002];
+		// 元素熟知
+		avaterElementMasteryList[avaterId] = element["fightPropMap"][28];
+		// 会心率
+		avaterCriticalPercentList[avaterId] = element["fightPropMap"][20];
+		// 会心ダメージ
+		avaterCriticalHurtPercentList[avaterId] = element["fightPropMap"][22];
+		// 元素チャージ効率
+		avaterElementChargeEfficiencyPercentList[avaterId] = element["fightPropMap"][23];
+		// 任意元素ダメージ
+		if(element["fightPropMap"][30] !== 0) {
+			avaterElementHurtPercentList[avaterId] = element["fightPropMap"][30];
+		} else if(element["fightPropMap"][40] !== 0 ) {
+			avaterElementHurtPercentList[avaterId] = element["fightPropMap"][40];
+		} else if(element["fightPropMap"][41] !== 0 ) {
+			avaterElementHurtPercentList[avaterId] = element["fightPropMap"][41];
+		} else if(element["fightPropMap"][42] !== 0 ) {
+			avaterElementHurtPercentList[avaterId] = element["fightPropMap"][42];
+		} else if(element["fightPropMap"][43] !== 0 ) {
+			avaterElementHurtPercentList[avaterId] = element["fightPropMap"][43];
+		} else if(element["fightPropMap"][44] !== 0 ) {
+			avaterElementHurtPercentList[avaterId] = element["fightPropMap"][44];
+		} else if(element["fightPropMap"][45] !== 0 ) {
+			avaterElementHurtPercentList[avaterId] = element["fightPropMap"][45];
+		} else if(element["fightPropMap"][46] !== 0 ) {
+			avaterElementHurtPercentList[avaterId] = element["fightPropMap"][46];
+		}
+	});
+
+	// 各連想配列の平均値を算出
+	let averageHp: number = 0;
+	let averageAttackPower: number = 0;
+	let averageDefensePower: number = 0;
+	let averageElementMastery: number = 0;
+	let averageCriticalPercent: number = 0;
+	let averageCriticalHurtPercent: number = 0;
+	let averageElementChargeEfficiencyPercent: number = 0;
+	let averageElementHurtPercent: number = 0;
+
+	// HP
+	for (let key in avaterHpList) { averageHp! += avaterHpList[key]; }
+	console.log(averageHp);
+	averageHp = Math.round(averageHp! / Object.keys(avaterHpList).length * 1000) / 1000;
+	// 攻撃力
+	for (let key in avaterAttackPowerList) { averageAttackPower! += avaterAttackPowerList[key]; }
+	averageAttackPower = Math.round(averageAttackPower! / Object.keys(avaterAttackPowerList).length * 1000) / 1000;
+	// 防御力
+	for (let key in avaterDefensePowerList) { averageDefensePower! += avaterDefensePowerList[key]; }
+	averageDefensePower = Math.round(averageDefensePower! / Object.keys(avaterDefensePowerList).length * 1000) / 1000;
+	// 元素熟知
+	for (let key in avaterElementMasteryList) { averageElementMastery! += avaterElementMasteryList[key]; }
+	averageElementMastery = Math.round(averageElementMastery! / Object.keys(avaterElementMasteryList).length * 1000) / 1000;
+	// 会心率
+	for (let key in avaterCriticalPercentList) { averageCriticalPercent! += avaterCriticalPercentList[key]; }
+	averageCriticalPercent = Math.round(averageCriticalPercent! / Object.keys(avaterCriticalPercentList).length * 1000) / 1000;
+	// 会心ダメージ
+	for (let key in avaterCriticalHurtPercentList) { averageCriticalHurtPercent! += avaterCriticalHurtPercentList[key]; }
+	averageCriticalHurtPercent = Math.round(averageCriticalHurtPercent! / Object.keys(avaterCriticalHurtPercentList).length * 1000) / 1000;
+	// 元素チャージ効率
+	for (let key in avaterElementChargeEfficiencyPercentList) { averageElementChargeEfficiencyPercent! += avaterElementChargeEfficiencyPercentList[key]; }
+	averageElementChargeEfficiencyPercent = Math.round(averageElementChargeEfficiencyPercent! / Object.keys(avaterElementChargeEfficiencyPercentList).length * 1000) / 1000;
+	// 任意元素ダメージ
+	for (let key in avaterElementHurtPercentList) { averageElementHurtPercent! += avaterElementHurtPercentList[key]; }
+	averageElementHurtPercent = Math.round(averageElementHurtPercent! / Object.keys(avaterElementHurtPercentList).length * 1000) / 1000;
+
+	return {
+		"nickname": nickname,
+		"hp": averageHp,
+		"attack": averageAttackPower,
+		"defense": averageDefensePower,
+		"element_mastery": averageElementMastery,
+		"critical_percent": averageCriticalPercent,
+		"critical_hurt_percent": averageCriticalHurtPercent,
+		"element_charge_efficiency_percent": averageElementChargeEfficiencyPercent,
+		"element_hurt_percent": averageElementHurtPercent
+	}
+}
 
 export default app
