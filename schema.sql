6056690ac8a2cc91e57ebf46ba7922d2e4f04ccf
chan-mai <yashinomi0804@gmail.com>
2024-06-03 10:48:53 +0900

    update: change schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,27 @@
 DROP TABLE IF EXISTS userdata;
 CREATE TABLE userdata (
     uid INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT NOT NULL,
-    hp NUMBER NOT NULL,
-	attack NUMBER NOT NULL,
-	defense NUMBER NOT NULL,
-	element_mastery NUMBER NOT NULL,
-	critical_percent NUMBER NOT NULL,
-	critical_hurt_percent NUMBER NOT NULL,
-	element_charge_efficiency_percent NUMBER NOT NULL,
-	element_hurt_percent NUMBER NOT NULL,
+	nickname TEXT NOT NULL,
     created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
     updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
+);
+
+DROP TABLE IF EXISTS characters;
+CREATE TABLE characters (
+	character_id INTEGER PRIMARY KEY,
+	uid INTEGER NOT NULL,
+	constellations INTEGER NOT NULL,
+	level INTEGER NOT NULL,
+	added_hp INTEGER NOT NULL,
+	added_attack INTEGER NOT NULL,
+	added_defense INTEGER NOT NULL,
+	critial_rate INTEGER NOT NULL,
+	critial_damage INTEGER NOT NULL,
+	charge_efficiency INTEGER NOT NULL,
+	elemental_mastery INTEGER NOT NULL,
+	elemental_type_name TEXT NOT NULL,
+	elemental_value INTEGER NOT NULL,
+	created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+	updated_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+	FOREIGN KEY (uid) REFERENCES userdata(uid)
 );
